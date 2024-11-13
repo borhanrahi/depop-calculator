6fa70788ec564d68b8bad9bd8e9163443b147117
borhanrahi <borhanrahi111@gmail.com>
2024-11-13 17:16:52 +0600

    fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -121,13 +121,6 @@ export default function RootLayout({
           strategy="lazy"
         />
         <link 
-          rel="preload" 
-          href="/fonts/your-font.woff2" 
-          as="font" 
-          type="font/woff2" 
-          crossOrigin="anonymous" 
-        />
-        <link 
           rel="preconnect" 
           href="https://fonts.gstatic.com" 
           crossOrigin="anonymous" 
